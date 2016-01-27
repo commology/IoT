b06eb08f3f896a9826d2f0e814aa00429c2404cb
commology <commology@gmail.com>
2016-01-27 20:44:54 +0800

    simply, DS18B02

--- DHT22_LCD12864/DHT22_LCD12864.ino
+++ DHT22_LCD12864/DHT22_LCD12864.ino
@@ -1,28 +1,42 @@
 /*
- * DHT22 -> LCD12864
- * using afafriut lib
+ * DHT22 -> LCD1602/LCD12864
+ * using afafriut DHT22 lib
  * U8glib Arduino
+ * DS18B02; with its specific small PCB
  */
 
 #include <LiquidCrystal.h>
 #include "U8glib.h"
 #include "DHT.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-#define DIV 5
+#define FREQ 5
 
 #define DHTPIN 2
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
 
-DHT dht(DHTPIN, DHTTYPE);
+#define ONE_WIRE_BUS 3
+
+
 LiquidCrystal lcd(8, 7, 6, 5, 4, 3);
 U8GLIB_SH1106_128X64 u8g(10, 9, 12, 11, 13);
 
 String dispbuf;
 
+
+DHT dht(DHTPIN, DHTTYPE);
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature ds18b02(&oneWire);
+
 unsigned long cyc = 0;
 unsigned long seq = 0;
 
-const unsigned long CYC_LIMIT = DIV * (1000L*1000L);
+const unsigned long CYC_LIMIT = FREQ * (1000L*1000L);
 
 void setup_timer() {
   // initialize timer1 
@@ -43,6 +57,7 @@ void setup() {
   
   Serial.begin(38400);
   dht.begin();
+  ds18b02.begin();
   
   // set up the LCD's number of columns and rows:
   //lcd.begin(16, 2);
@@ -60,7 +75,7 @@ ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
   if (cyc >= CYC_LIMIT)
     cyc = 1;
   
-  if (cyc % DIV != 1)
+  if (cyc % FREQ != 1)
     return;
 
   seq++;
@@ -70,52 +85,63 @@ ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
   float h = dht.readHumidity();
   // Read temperature as Celsius
   float t = dht.readTemperature();
-  
+   
+  // call sensors.requestTemperatures() to issue a global temperature 
+  // request to all devices on the bus
+  ds18b02.requestTemperatures();      // Send the command to get temperatures
+  // After we got the temperatures, we can print them here.
+  // We use the function ByIndex, and as an example get the temperature from the first sensor only.
+  float T = ds18b02.getTempCByIndex(0);   // 1-wire device index
+ 
   // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(t)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
 
-  Serial.print("DHT,");
+  Serial.print("AMS,");
   Serial.print(seq);
   Serial.print(",");
-  Serial.print(h);
+  Serial.print(h, 1);
   Serial.print(",");
-  Serial.print(t);
+  Serial.print(t, 1);
+  Serial.print(",");
+  Serial.print(T);
   Serial.println();
   Serial.flush();
- 
+
   // set the cursor to column 0, line 0
   lcd.setCursor(0, 0);
-  lcd.print("T = ");
-  lcd.print(t);
-  lcd.print("C");
-
+  lcd.print("H = ");
+  lcd.print(h, 1);
+  lcd.print("%");
+ 
   // set the cursor to column 0, line 1
   lcd.setCursor(0, 1);
-  lcd.print("H = ");
-  lcd.print(h);
-  lcd.print("%");
+  lcd.print("T = ");
+  lcd.print(t, 1);
+  lcd.print("C");
 
   u8g.firstPage();
   do 
   {
-    dispbuf = "T = ";
-    dispbuf.concat(t);
-    dispbuf += "C";
+    dispbuf = "# = ";
+    dispbuf.concat(seq);
     u8g.drawStr(0, 0, dispbuf.c_str());
 
-    dispbuf = "H = ";
+    dispbuf = "";
     dispbuf.concat(h);
-    dispbuf += "%"; 
+    dispbuf += "% ";
+    dispbuf.concat(t);
+    dispbuf += "C"; 
     u8g.drawStr(0, 14, dispbuf.c_str());
 
-    dispbuf = "Q = ";
-    dispbuf.concat(seq);
+    dispbuf = "T! = ";
+    dispbuf.concat(T);
     u8g.drawStr(0, 28, dispbuf.c_str());
   } while(u8g.nextPage());
 }
+
 void loop() {
   // Wait a few seconds between measurements.
 }
